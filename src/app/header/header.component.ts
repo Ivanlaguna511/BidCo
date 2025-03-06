794c597fd74d573778507864af2d52dd054ef1a9
Gonza5000 <5000.gonza.5000@gmail.com>
2025-03-06 13:26:08 +0100

    desplegabke en la cabecera

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+    dropdownOpen = false;
+
+    toggleDropdown() {
+        this.dropdownOpen = !this.dropdownOpen;
+    }
+
+    closeDropdown() {
+        this.dropdownOpen = false;
+    }
 }
