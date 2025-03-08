5f54880d7262245aa0cb674ead1500fce2f998e2
Gonza5000 <5000.gonza.5000@gmail.com>
2025-03-08 12:32:04 +0100

    Creacion product-details para los detalles de los productos en la puja Creacion de product-item para la lista de productos Product-details practicamente terminado de forma estatica

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    HeaderComponent,
+    ProductDetailsComponent],
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
