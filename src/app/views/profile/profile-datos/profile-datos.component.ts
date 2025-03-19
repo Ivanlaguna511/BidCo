e053fde84201897dad26b74f637c9e89f85e28f1
Gonza5000 <5000.gonza.5000@gmail.com>
2025-03-19 19:08:00 +0100

    mas cosas

--- src/app/views/profile/profile-datos/profile-datos.component.ts
+++ src/app/views/profile/profile-datos/profile-datos.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
-import { DATA_USER } from '../../../datos_estaticos/user_estadisticas';
+import { DATA_USER, DATA_EXPERT } from '../../../datos_estaticos/user_estadisticas';
+import { AuthService } from '../../../auth.service';
 
 @Component({
   selector: 'app-profile-datos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-datos.component.html',
   styleUrl: './profile-datos.component.css'
 })
 export class ProfileDatosComponent {
     user = DATA_USER;
+    expert = DATA_EXPERT;
+    isExpert = false;
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {
+        this.authService.userRole$.subscribe((estado) => {
+            this.isExpert = estado === 'expert';
+        });
+    }
 }
--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+
 import { FooterComponent } from "../../components/footer/footer.component";
 import { HeaderComponent } from "../../components/header/header.component";
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [
+    RouterLink, 
+    RouterOutlet, 
+    FooterComponent, 
+    HeaderComponent, 
+    CommonModule
+],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 
-export class ProfileComponent {}
+export class ProfileComponent {
+    isExpert = false;
+    
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {
+        this.authService.userRole$.subscribe((estado) => {
+            this.isExpert = estado === 'expert';
+        });
+    }
+}
