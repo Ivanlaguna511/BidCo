999734fcac45604851b7c2000ab5ec2f715cdad9
Iván <moroi5251@gmail.com>
2026-03-16 22:08:46 +0100

    Lanzamiento completo en la nube

--- backend/src/main/java/com/bidco/api_rest/service/impl/UsuarioServiceImpl.java
+++ backend/src/main/java/com/bidco/api_rest/service/impl/UsuarioServiceImpl.java
@@ -105,7 +105,7 @@ public class UsuarioServiceImpl implements UsuarioService {
         }
         Usuario usuario = usuarioOpt.get();
         // Validar la contraseña (en producción, utiliza hashing)
-        if (!usuario.getContraseña().equals(loginDTO.getContraseña())) {
+        if (!usuario.getContraseña().equals(loginDTO.getContrasena())) {
             throw new IllegalArgumentException("Credenciales incorrectas");
         }
         // Generar el token JWT usando el ID del usuario (se guarda en el claim "sub")
