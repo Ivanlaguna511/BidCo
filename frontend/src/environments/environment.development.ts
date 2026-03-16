63bd81be02d7c3af3331835fe1f41f855d1896e4
Iván <moroi5251@gmail.com>
2026-03-16 17:02:07 +0100

    Lanzamiento completo en la nube

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://bidco-backend.onrender.com/api'
-  // Debería ser esta para pruebas apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://localhost:8080/api'
 };
