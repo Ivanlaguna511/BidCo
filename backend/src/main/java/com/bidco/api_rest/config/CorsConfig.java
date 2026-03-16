2a7027cc8dca930091eb6183cb8109ce27425090
Iván <moroi5251@gmail.com>
2026-03-16 22:20:44 +0100

    Lanzamiento completo en la nube fix

--- backend/src/main/java/com/bidco/api_rest/config/CorsConfig.java
+++ backend/src/main/java/com/bidco/api_rest/config/CorsConfig.java
@@ -11,8 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // Permitimos local y dejamos abierta la puerta a Vercel
-                .allowedOriginPatterns("http://localhost:4200", "https://*.vercel.app") 
+                // CAMBIO AQUÍ: Usamos allowedOrigins y la URL exacta
+                .allowedOrigins("http://localhost:4200", "https://bid-co.vercel.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
@@ -20,8 +20,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Esto sirve para servir las imágenes que subas
-        // OJO: En Render los archivos se borran al reiniciar si no usas un "Disk"
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
     }
